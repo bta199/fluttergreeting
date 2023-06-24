cca1405a0d9d993cd54debaa16766291148b6913
Ben Taylor <bta199@gmail.com>
2023-06-25 11:07:18 +1200

    SecurityContext not available in web. Remove build folder.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'my_app.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 main() async {
   runApp(const MyApp());
@@ -9,8 +10,10 @@ main() async {
 }
 
 Future<void> initialize() async {
-  final ByteData certificateData =
-      await rootBundle.load('assets/randomuser.me.crt');
-  SecurityContext.defaultContext
-      .setTrustedCertificatesBytes(certificateData.buffer.asUint8List());
+  if (!kIsWeb) {
+    final ByteData certificateData =
+        await rootBundle.load('assets/randomuser.me.crt');
+    SecurityContext.defaultContext
+        .setTrustedCertificatesBytes(certificateData.buffer.asUint8List());
+  }
 }
